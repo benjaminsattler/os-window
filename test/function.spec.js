423f7b86438775e65c0760d78ae7b014e92c217e
Benjamin Sattler <bsattler.inbox@gmail.com>
2020-05-03 17:57:29 +0200

    Optimize shadow dom generation with template element

--- test/function.spec.js
+++ test/function.spec.js
@@ -19,14 +19,14 @@ describe('maximize button', () => {
   describe('when non interactive', () => {
     it('does not change the window-state property', () => {
       sut.windowState = 'minimized';
-      const maximizeButton = sut.shadow.querySelector('.window-title-button__maximize');
+      const maximizeButton = sut.__shadow.querySelector('.window-title-button__maximize');
       maximizeButton.click();
       chai.expect(sut.getAttribute('window-state')).to.equal('minimized');
     });
 
     it('does not change the windowState attribute', () => {
       sut.windowState = 'minimized';
-      const maximizeButton = sut.shadow.querySelector('.window-title-button__maximize');
+      const maximizeButton = sut.__shadow.querySelector('.window-title-button__maximize');
       maximizeButton.click();
       chai.expect(sut.windowState).to.equal('minimized');
     });
@@ -36,7 +36,7 @@ describe('maximize button', () => {
     it('changes the window-state property', () => {
       sut.interactive = true;
       sut.windowState = 'minimized';
-      const maximizeButton = sut.shadow.querySelector('.window-title-button__maximize');
+      const maximizeButton = sut.__shadow.querySelector('.window-title-button__maximize');
       maximizeButton.click();
       chai.expect(sut.getAttribute('window-state')).to.equal('maximized');
     });
@@ -44,7 +44,7 @@ describe('maximize button', () => {
     it('changes the windowState attribute', () => {
       sut.interactive = true;
       sut.windowState = 'minimized';
-      const maximizeButton = sut.shadow.querySelector('.window-title-button__maximize');
+      const maximizeButton = sut.__shadow.querySelector('.window-title-button__maximize');
       maximizeButton.click();
       chai.expect(sut.windowState).to.equal('maximized');
     });
@@ -52,9 +52,9 @@ describe('maximize button', () => {
     it('maximizes the minimized window', () => {
       sut.interactive = true;
       sut.windowState = 'minimized';
-      const maximizeButton = sut.shadow.querySelector('.window-title-button__maximize');
+      const maximizeButton = sut.__shadow.querySelector('.window-title-button__maximize');
       maximizeButton.click();
-      const slotWrapper = sut.shadow.querySelector('.window-content-slot-wrapper');
+      const slotWrapper = sut.__shadow.querySelector('.window-content-slot-wrapper');
       const slotWrapperHeight = window.getComputedStyle(slotWrapper).height;
       chai.expect(slotWrapperHeight).to.not.equal('0px');
     });
@@ -65,14 +65,14 @@ describe('minimize button', () => {
   describe('when non interactive', () => {
     it('does not change the window-state property', () => {
       sut.windowState = 'maximized';
-      const minimizeButton = sut.shadow.querySelector('.window-title-button__minimize');
+      const minimizeButton = sut.__shadow.querySelector('.window-title-button__minimize');
       minimizeButton.click();
       chai.expect(sut.getAttribute('window-state')).to.equal('maximized');
     });
 
     it('does not change the windowState attribute', () => {
       sut.windowState = 'maximized';
-      const minimizeButton = sut.shadow.querySelector('.window-title-button__minimize');
+      const minimizeButton = sut.__shadow.querySelector('.window-title-button__minimize');
       minimizeButton.click();
       chai.expect(sut.windowState).to.equal('maximized');
     });
@@ -82,7 +82,7 @@ describe('minimize button', () => {
     it('changes the window-state property', () => {
       sut.interactive = true;
       sut.windowState = 'maximized';
-      const minimizeButton = sut.shadow.querySelector('.window-title-button__minimize');
+      const minimizeButton = sut.__shadow.querySelector('.window-title-button__minimize');
       minimizeButton.click();
       chai.expect(sut.getAttribute('window-state')).to.equal('minimized');
     });
@@ -90,7 +90,7 @@ describe('minimize button', () => {
     it('changes the windowState attribute', () => {
       sut.interactive = true;
       sut.windowState = 'maximized';
-      const minimizeButton = sut.shadow.querySelector('.window-title-button__minimize');
+      const minimizeButton = sut.__shadow.querySelector('.window-title-button__minimize');
       minimizeButton.click();
       chai.expect(sut.windowState).to.equal('minimized');
     });
@@ -98,9 +98,9 @@ describe('minimize button', () => {
     it('minimizes the maximized window', () => {
       sut.interactive = true;
       sut.windowState = 'maximized';
-      const minimizeButton = sut.shadow.querySelector('.window-title-button__minimize');
+      const minimizeButton = sut.__shadow.querySelector('.window-title-button__minimize');
       minimizeButton.click();
-      const slotWrapper = sut.shadow.querySelector('.window-content-slot-wrapper');
+      const slotWrapper = sut.__shadow.querySelector('.window-content-slot-wrapper');
       const slotWrapperHeight = window.getComputedStyle(slotWrapper).height;
       chai.expect(slotWrapperHeight).to.equal('0px');
     });
--- test/theme.spec.js
+++ test/theme.spec.js
@@ -1,7 +1,10 @@
 /* eslint-disable camelcase */
 
 function getStyles(win, selector, pseudoelement) {
-  return window.getComputedStyle(win.toDomElement().shadow.querySelector(selector), pseudoelement);
+  return window.getComputedStyle(
+    win.toDomElement().__shadow.querySelector(selector),
+    pseudoelement,
+  );
 }
 
 function assertStylesEqual(styles1, styles2) {
@@ -131,20 +134,20 @@ describe('themes', () => {
 
       describe('close button', () => {
         it('has correct position', () => {
-          const closeButton = mac_light.toDomElement().shadow.querySelector('.window-title-button__close');
+          const closeButton = mac_light.toDomElement().__shadow.querySelector('.window-title-button__close');
           chai.expect(closeButton.offsetLeft).to.equal(3);
           // TODO: This assertion breaks in CI
           // chai.expect(closeButton.offsetTop).to.equal(14);
         });
 
         it('has correct background color', () => {
-          const closeButton = mac_light.toDomElement().shadow.querySelector('.window-title-button__close');
+          const closeButton = mac_light.toDomElement().__shadow.querySelector('.window-title-button__close');
           const backgroundColor = window.getComputedStyle(closeButton).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(239, 83, 74)');
         });
 
         it('has correct foreground color', () => {
-          const closeButton = mac_light.toDomElement().shadow.querySelector('.window-title-button__close');
+          const closeButton = mac_light.toDomElement().__shadow.querySelector('.window-title-button__close');
           const foregroundColor = window.getComputedStyle(closeButton).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgba(0, 0, 0, 0)');
         });
@@ -152,20 +155,20 @@ describe('themes', () => {
 
       describe('minimize button', () => {
         it('has correct position', () => {
-          const minimizeButton = mac_light.toDomElement().shadow.querySelector('.window-title-button__minimize');
+          const minimizeButton = mac_light.toDomElement().__shadow.querySelector('.window-title-button__minimize');
           chai.expect(minimizeButton.offsetLeft).to.equal(24);
           // TODO: This assertion breaks in CI
           // chai.expect(minimizeButton.offsetTop).to.equal(14);
         });
 
         it('has correct background color', () => {
-          const minimizeButton = mac_light.toDomElement().shadow.querySelector('.window-title-button__minimize');
+          const minimizeButton = mac_light.toDomElement().__shadow.querySelector('.window-title-button__minimize');
           const backgroundColor = window.getComputedStyle(minimizeButton).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(237, 192, 74)');
         });
 
         it('has correct foreground color', () => {
-          const minimizeButton = mac_light.toDomElement().shadow.querySelector('.window-title-button__minimize');
+          const minimizeButton = mac_light.toDomElement().__shadow.querySelector('.window-title-button__minimize');
           const foregroundColor = window.getComputedStyle(minimizeButton).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgba(0, 0, 0, 0)');
         });
@@ -173,20 +176,20 @@ describe('themes', () => {
 
       describe('maximize button', () => {
         it('has correct position', () => {
-          const maximizeButton = mac_light.toDomElement().shadow.querySelector('.window-title-button__maximize');
+          const maximizeButton = mac_light.toDomElement().__shadow.querySelector('.window-title-button__maximize');
           chai.expect(maximizeButton.offsetLeft).to.equal(46);
           // TODO: This assertion breaks in CI
           // chai.expect(maximizeButton.offsetTop).to.equal(14);
         });
 
         it('has correct background color', () => {
-          const maximizeButton = mac_light.toDomElement().shadow.querySelector('.window-title-button__maximize');
+          const maximizeButton = mac_light.toDomElement().__shadow.querySelector('.window-title-button__maximize');
           const backgroundColor = window.getComputedStyle(maximizeButton).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(151, 210, 114)');
         });
 
         it('has correct foreground color', () => {
-          const minimizeButton = mac_light.toDomElement().shadow.querySelector('.window-title-button__minimize');
+          const minimizeButton = mac_light.toDomElement().__shadow.querySelector('.window-title-button__minimize');
           const foregroundColor = window.getComputedStyle(minimizeButton).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgba(0, 0, 0, 0)');
         });
@@ -194,19 +197,19 @@ describe('themes', () => {
 
       describe('window title bar', () => {
         it('has correct height', () => {
-          const titleBar = mac_light.toDomElement().shadow.querySelector('.window-title-bar');
+          const titleBar = mac_light.toDomElement().__shadow.querySelector('.window-title-bar');
           const height = window.getComputedStyle(titleBar).getPropertyValue('height');
           chai.expect(height).to.equal('40px');
         });
 
         it('has correct background color', () => {
-          const titleBar = mac_light.toDomElement().shadow.querySelector('.window-title-bar');
+          const titleBar = mac_light.toDomElement().__shadow.querySelector('.window-title-bar');
           const backgroundColor = window.getComputedStyle(titleBar).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(238, 241, 245)');
         });
 
         it('has correct foreground color', () => {
-          const titleBar = mac_light.toDomElement().shadow.querySelector('.window-title-bar');
+          const titleBar = mac_light.toDomElement().__shadow.querySelector('.window-title-bar');
           const foregroundColor = window.getComputedStyle(titleBar).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgb(135, 130, 125)');
         });
@@ -214,37 +217,37 @@ describe('themes', () => {
 
       describe('window content slot wrapper', () => {
         it('has correct spacing left', () => {
-          const slotWrapper = mac_light.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_light.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const paddingLeft = window.getComputedStyle(slotWrapper).getPropertyValue('padding-left');
           chai.expect(paddingLeft).to.equal('16px');
         });
 
         it('has correct spacing right', () => {
-          const slotWrapper = mac_light.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_light.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const paddingRight = window.getComputedStyle(slotWrapper).getPropertyValue('padding-right');
           chai.expect(paddingRight).to.equal('16px');
         });
 
         it('has correct spacing top', () => {
-          const slotWrapper = mac_light.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_light.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const paddingTop = window.getComputedStyle(slotWrapper).getPropertyValue('padding-top');
           chai.expect(paddingTop).to.equal('0px');
         });
 
         it('has correct spacing bottom', () => {
-          const slotWrapper = mac_light.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_light.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const paddingBottom = window.getComputedStyle(slotWrapper).getPropertyValue('padding-bottom');
           chai.expect(paddingBottom).to.equal('8px');
         });
 
         it('has correct background color', () => {
-          const slotWrapper = mac_light.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_light.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const backgroundColor = window.getComputedStyle(slotWrapper).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(238, 241, 245)');
         });
 
         it('has correct foreground color', () => {
-          const slotWrapper = mac_light.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_light.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const foregroundColor = window.getComputedStyle(slotWrapper).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgb(135, 130, 125)');
         });
@@ -254,20 +257,20 @@ describe('themes', () => {
     describe('dark theme', () => {
       describe('close button', () => {
         it('has correct position', () => {
-          const closeButton = mac_dark.toDomElement().shadow.querySelector('.window-title-button__close');
+          const closeButton = mac_dark.toDomElement().__shadow.querySelector('.window-title-button__close');
           chai.expect(closeButton.offsetLeft).to.equal(3);
           // TODO: This assertion breaks in CI
           // chai.expect(closeButton.offsetTop).to.equal(14);
         });
 
         it('has correct background color', () => {
-          const closeButton = mac_dark.toDomElement().shadow.querySelector('.window-title-button__close');
+          const closeButton = mac_dark.toDomElement().__shadow.querySelector('.window-title-button__close');
           const backgroundColor = window.getComputedStyle(closeButton).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(239, 83, 74)');
         });
 
         it('has correct foreground color', () => {
-          const closeButton = mac_dark.toDomElement().shadow.querySelector('.window-title-button__close');
+          const closeButton = mac_dark.toDomElement().__shadow.querySelector('.window-title-button__close');
           const foregroundColor = window.getComputedStyle(closeButton).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgba(0, 0, 0, 0)');
         });
@@ -275,20 +278,20 @@ describe('themes', () => {
 
       describe('minimize button', () => {
         it('has correct position', () => {
-          const minimizeButton = mac_dark.toDomElement().shadow.querySelector('.window-title-button__minimize');
+          const minimizeButton = mac_dark.toDomElement().__shadow.querySelector('.window-title-button__minimize');
           chai.expect(minimizeButton.offsetLeft).to.equal(24);
           // TODO: This assertion breaks in CI
           // chai.expect(minimizeButton.offsetTop).to.equal(14);
         });
 
         it('has correct background color', () => {
-          const minimizeButton = mac_dark.toDomElement().shadow.querySelector('.window-title-button__minimize');
+          const minimizeButton = mac_dark.toDomElement().__shadow.querySelector('.window-title-button__minimize');
           const backgroundColor = window.getComputedStyle(minimizeButton).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(237, 192, 74)');
         });
 
         it('has correct foreground color', () => {
-          const minimizeButton = mac_dark.toDomElement().shadow.querySelector('.window-title-button__minimize');
+          const minimizeButton = mac_dark.toDomElement().__shadow.querySelector('.window-title-button__minimize');
           const foregroundColor = window.getComputedStyle(minimizeButton).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgba(0, 0, 0, 0)');
         });
@@ -296,20 +299,20 @@ describe('themes', () => {
 
       describe('maximize button', () => {
         it('has correct position', () => {
-          const maximizeButton = mac_dark.toDomElement().shadow.querySelector('.window-title-button__maximize');
+          const maximizeButton = mac_dark.toDomElement().__shadow.querySelector('.window-title-button__maximize');
           chai.expect(maximizeButton.offsetLeft).to.equal(46);
           // TODO: This assertion breaks in CI
           // chai.expect(maximizeButton.offsetTop).to.equal(14);
         });
 
         it('has correct background color', () => {
-          const maximizeButton = mac_dark.toDomElement().shadow.querySelector('.window-title-button__maximize');
+          const maximizeButton = mac_dark.toDomElement().__shadow.querySelector('.window-title-button__maximize');
           const backgroundColor = window.getComputedStyle(maximizeButton).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(151, 210, 114)');
         });
 
         it('has correct foreground color', () => {
-          const minimizeButton = mac_dark.toDomElement().shadow.querySelector('.window-title-button__minimize');
+          const minimizeButton = mac_dark.toDomElement().__shadow.querySelector('.window-title-button__minimize');
           const foregroundColor = window.getComputedStyle(minimizeButton).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgba(0, 0, 0, 0)');
         });
@@ -317,19 +320,19 @@ describe('themes', () => {
 
       describe('window title bar', () => {
         it('has correct height', () => {
-          const titleBar = mac_dark.toDomElement().shadow.querySelector('.window-title-bar');
+          const titleBar = mac_dark.toDomElement().__shadow.querySelector('.window-title-bar');
           const height = window.getComputedStyle(titleBar).getPropertyValue('height');
           chai.expect(height).to.equal('40px');
         });
 
         it('has correct background color', () => {
-          const titleBar = mac_dark.toDomElement().shadow.querySelector('.window-title-bar');
+          const titleBar = mac_dark.toDomElement().__shadow.querySelector('.window-title-bar');
           const backgroundColor = window.getComputedStyle(titleBar).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(52, 58, 70)');
         });
 
         it('has correct foreground color', () => {
-          const titleBar = mac_dark.toDomElement().shadow.querySelector('.window-title-bar');
+          const titleBar = mac_dark.toDomElement().__shadow.querySelector('.window-title-bar');
           const foregroundColor = window.getComputedStyle(titleBar).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgb(255, 255, 255)');
         });
@@ -337,37 +340,37 @@ describe('themes', () => {
 
       describe('window content slot wrapper', () => {
         it('has correct spacing left', () => {
-          const slotWrapper = mac_dark.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_dark.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const paddingLeft = window.getComputedStyle(slotWrapper).getPropertyValue('padding-left');
           chai.expect(paddingLeft).to.equal('16px');
         });
 
         it('has correct spacing right', () => {
-          const slotWrapper = mac_dark.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_dark.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const paddingRight = window.getComputedStyle(slotWrapper).getPropertyValue('padding-right');
           chai.expect(paddingRight).to.equal('16px');
         });
 
         it('has correct spacing top', () => {
-          const slotWrapper = mac_dark.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_dark.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const paddingTop = window.getComputedStyle(slotWrapper).getPropertyValue('padding-top');
           chai.expect(paddingTop).to.equal('0px');
         });
 
         it('has correct spacing bottom', () => {
-          const slotWrapper = mac_dark.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_dark.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const paddingBottom = window.getComputedStyle(slotWrapper).getPropertyValue('padding-bottom');
           chai.expect(paddingBottom).to.equal('8px');
         });
 
         it('has correct background color', () => {
-          const slotWrapper = mac_dark.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_dark.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const backgroundColor = window.getComputedStyle(slotWrapper).getPropertyValue('background-color');
           chai.expect(backgroundColor).to.equal('rgb(52, 58, 70)');
         });
 
         it('has correct foreground color', () => {
-          const slotWrapper = mac_dark.toDomElement().shadow.querySelector('.window-content-slot-wrapper');
+          const slotWrapper = mac_dark.toDomElement().__shadow.querySelector('.window-content-slot-wrapper');
           const foregroundColor = window.getComputedStyle(slotWrapper).getPropertyValue('color');
           chai.expect(foregroundColor).to.equal('rgb(255, 255, 255)');
         });
